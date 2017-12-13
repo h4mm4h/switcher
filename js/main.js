e8170ae098443a0fd5ca468a2dd266ab1c071d21
h4mm4h <h4mm4h@h4mm4hs-MacBook-Pro.local>
2017-12-13 16:53:56 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
 ;(function() {
 	"use strict";
 
+
+	var parent = document.getElementById('ba-body');
+	var input = document.getElementById('ba-input');
+	var view = document.getElementById('ba-view');
+	var activeClass = 'active';
+
+	function handleView(e){
+	  // ctrl + e
+	  if (e.ctrlKey && e.keyCode === 69) showEditor();
+	  // ctrl + s
+	  if (e.ctrlKey && e.keyCode === 83) saveInputText();
+	  // esc
+	  if (e.keyCode === 27) exitEditor();
+	}
+
+	function saveInputText() {
+	  parent.classList.remove(activeClass);
+	  view.innerHTML = input.value;
+	}
+
+	function showEditor() {
+	  parent.classList.add(activeClass);
+	  input.value = view.innerHTML;
+	}
+
+	function exitEditor() {
+	  parent.classList.remove(activeClass);
+	}
+
+
+	document.addEventListener('keydown', handleView );
+
+
+
 })();
 
